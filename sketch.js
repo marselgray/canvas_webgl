cb8aca84fc65ebc5cc226300ec66273a906047d9
Marsel Gray <marselgray@gmail.com>
2021-11-08 14:33:19 -0500

    Lesson: canvas API basics

--- sketch.js
+++ sketch.js
@@ -6,8 +6,17 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'orange';
     context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
+    context.fillStyle = 'yellow';
+    context.fill();
+
+    context.lineWidth = 20;
+    context.strokeStyle = 'green';
+    context.stroke();
   };
 };
 
