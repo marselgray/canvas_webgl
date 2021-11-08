13748625fb733623793b6b3626b1f124782e6922
Marsel Gray <marselgray@gmail.com>
2021-11-08 15:19:08 -0500

    Lesson: canvas sketch applications

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# canvas_webgl
+# Canvas Sketch Notes
+
+
+## Install the CLI tool globally
+npm install canvas-sketch-cli -g
+
+
+## Scaffold a new 'sketch.js' file and open the browser
+canvas-sketch sketch.js --open
--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048],
+  // orientation: 'landscape',
+  // units: 'cm',
+  // pixelsPerInch: 300
 };
 
 const sketch = () => {
@@ -10,12 +13,12 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     context.beginPath();
-    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
-    context.fillStyle = 'yellow';
+    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
+    context.fillStyle = 'red';
     context.fill();
 
-    context.lineWidth = 20;
-    context.strokeStyle = 'green';
+    context.lineWidth = width * 0.05;
+    context.strokeStyle = 'blue';
     context.stroke();
   };
 };
